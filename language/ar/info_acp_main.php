bdab2718de67ad202aaccaebda6c8f266199b14f
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-01-24 18:08:34 +0300

    Update info_acp_main.php

--- language/ar/info_acp_main.php
+++ language/ar/info_acp_main.php
@@ -5,7 +5,7 @@
 * @copyright (c) 2015 Martin ( https://github.com/Martin-G- )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
-* Translated By : Basil Taha Alhitary - www.alhitary.net
+* Translated By : Bassel Taha Alhitary - www.alhitary.net
 */
 
 if (!defined('IN_PHPBB'))
@@ -19,18 +19,18 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_EMPTYPOSTSUBJECTS_TITLE'													=> 'TODO Empty Post Subjects',
+	'ACP_EMPTYPOSTSUBJECTS_TITLE'													=> 'تخصيص عنوان المشاركة',
 	'ACP_EMPTYPOSTSUBJECTS_SETTINGS'												=> 'الإعدادات',
 	'ACP_EMPTYPOSTSUBJECTS_SETTING_SAVED'											=> 'تم حفظ الإعدادات بنجاح !',
-	'ACP_EMPTYPOSTSUBJECTS_REPLY'													=> 'TODO Empty post subject on reply',
-	'ACP_EMPTYPOSTSUBJECTS_REPLY_EXPLAIN'											=> 'TODO Empties the \'Subject\' field in post editor when replying to a topic.',
-	'ACP_EMPTYPOSTSUBJECTS_QUICK_REPLY'												=> 'TODO Empty post subject on quick reply',
-	'ACP_EMPTYPOSTSUBJECTS_QUICK_REPLY_EXPLAIN'										=> 'TODO Empties the \'Subject\' field in the quick reply editor when viewing a topic.',
+	'ACP_EMPTYPOSTSUBJECTS_REPLY'													=> 'حذف عنوان المشاركة عند إضافة رد ',
+	'ACP_EMPTYPOSTSUBJECTS_REPLY_EXPLAIN'											=> 'حذف حقل \'العنوان\' في محرر الكتابة عند إضافة رد للموضوع.',
+	'ACP_EMPTYPOSTSUBJECTS_QUICK_REPLY'												=> 'حذف عنوان المشاركة في الرد السريع ',
+	'ACP_EMPTYPOSTSUBJECTS_QUICK_REPLY_EXPLAIN'										=> 'حذف حقل \'العنوان\' في الرد السريع عند مُشاهدة الموضوع.',
 	'ACP_EMPTYPOSTSUBJECTS_LAST_POST'												=> 'إظهار رابط آخر مُشاركة ',
 	'ACP_EMPTYPOSTSUBJECTS_LAST_POST_EXPLAIN'										=> 'تحديد طريقة إظهار رابط آخر مُشاركة في الصفحة الرئيسية للمنتدى.',
-	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_POST_SUBJECT				=> 'TODO subject of the post',
-	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_TOPIC_TITLE					=> 'TODO title of the topic that contains the post',
-	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_POST_SUBJECT_IF_NOT_EMPTY	=> 'TODO subject of the post if it is not empty, topic title otherwise',
-	'ACP_EMPTYPOSTSUBJECTS_SEARCH'													=> 'TODO Search result titles display',
-	'ACP_EMPTYPOSTSUBJECTS_SEARCH_EXPLAIN'											=> 'TODO Customize which text the titles in the search result display.',
+	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_POST_SUBJECT				=> 'عنوان آخر مُشاركة',
+	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_TOPIC_TITLE					=> 'عنوان الموضوع (الذي يحتوي على آخر مُشاركة)',
+	'ACP_EMPTYPOSTSUBJECTS_OPTION_' . EMPTYPOSTSUBJECTS_POST_SUBJECT_IF_NOT_EMPTY	=> 'عنوان آخر مشاركة, وإذا كان فارغاً يُستبدل بعنوان الموضوع',
+	'ACP_EMPTYPOSTSUBJECTS_SEARCH'													=> 'إظهار عناوين نتائج البحث ',
+	'ACP_EMPTYPOSTSUBJECTS_SEARCH_EXPLAIN'											=> 'تحديد طريقة إظهار رابط العنوان في نتائج البحث.',
 ));
