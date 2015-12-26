583d81b2267d4a8b2c2f91c3200aa6f12ec995ec
Martin G <martin.g@posteo.de>
2015-12-26 12:52:16 +0100

    Finish unit tests

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -10,16 +10,13 @@
 namespace martin\emptypostsubjects\tests\event;
 
 require_once dirname(__FILE__) . '/../../../../../includes/functions.php';
-// TODO: remove, probably not needed
-// require_once dirname(__FILE__) . '/../../../../../includes/functions_content.php';
 
 class listener_test extends \phpbb_test_case
 {
 	/** @var \martin\emptypostsubjects\event\listener */
 	protected $listener;
 
-	protected $template, $config, $user, $auth;
-	protected $auth_acl_map_admin, $auth_acl_map_user;
+	protected $template, $config;
 
 	/**
 	* Define the extensions to be tested
@@ -39,37 +36,11 @@ class listener_test extends \phpbb_test_case
 		parent::setUp();
 
 		$this->config = new \phpbb\config\config(array(
-			'display_last_subject'							=> 1,
 			'martin_emptypostsubjects_empty_reply'			=> 1,
 			'martin_emptypostsubjects_empty_quick_reply'	=> 1,
-			'martin_emptypostsubjects_last_post'			=> 2,
-			'martin_emptypostsubjects_search'				=> 2,
 		));
 
-		// TODO: wohl unnötig, genau wie die set_auth() methode, wenn
-		// foren mit unterschiedlichen rechten in den test-daten eingerichtet werden
-		$this->auth_acl_map_user = array(
-			array('f_read', 1, true),
-			array('f_read', '1', true),
-			array('f_read', 42, false),
-			array('f_read', '42', false),
-			array('a_', null, false),
-		);
-		$this->auth_acl_map_admin = array(
-			array('f_read', 1, true),
-			array('f_read', '1', true),
-			array('f_read', 42, true),
-			array('f_read', '42', true),
-			array('a_', null, true),
-		);
-
-		$this->create_auth();
-
 		$this->template = $this->getMockBuilder('\phpbb\template\template')
-			->disableOriginalConstructor()
-			->getMock();
-
-		$this->user = $this->getMockBuilder('\phpbb\user')
 			->disableOriginalConstructor()
 			->getMock();
 	}
@@ -81,32 +52,8 @@ class listener_test extends \phpbb_test_case
 	{
 		$this->listener = new \martin\emptypostsubjects\event\listener(
 			$this->template,
-			$this->config,
-			$this->user,
-			$this->auth
+			$this->config
 		);
-	}
-
-	/**
-	* Create auth object for tests. Needed to test different auth maps within one test.
-	*/
-	protected function create_auth()
-	{
-		$this->auth = $this->getMockBuilder('\phpbb\auth\auth')
-			->disableOriginalConstructor()
-			->getMock();
-	}
-
-	/**
-	* Setup auth object with auth map
-	*/
-	protected function set_auth($map)
-	{
-		$this->auth->expects($this->any())
-			->method('acl_get')
-			->with($this->stringContains('_'),
-				$this->anything())
-			->will($this->returnValueMap($map));
 	}
 
 	/**
@@ -129,7 +76,7 @@ class listener_test extends \phpbb_test_case
 			'core.viewtopic_modify_page_title',
 			'core.display_forums_modify_sql',
 			'core.display_forums_modify_forum_rows',
-			'core.display_forums_modify_template_vars',
+			'core.display_forums_before',
 			'core.search_modify_tpl_ary',
 		), array_keys(\martin\emptypostsubjects\event\listener::getSubscribedEvents()));
 	}
@@ -261,7 +208,7 @@ class listener_test extends \phpbb_test_case
 					'LEFT_JOIN'	=> array(
 						'some joins we do not care about',
 					),
-					'SELECT' => 'and some selects',
+					'SELECT'	=> 'and some selects',
 				),
 				array(
 					'LEFT_JOIN'	=> array(
@@ -271,7 +218,7 @@ class listener_test extends \phpbb_test_case
 							'ON'	=> "f.forum_last_post_id = t.topic_last_post_id AND t.topic_moved_id = 0"
 						),
 					),
-					'SELECT' => 'and some selects, t.topic_title',
+					'SELECT'	=> 'and some selects, t.topic_title AS last_post_topic_title',
 				),
 			),
 		);
@@ -299,49 +246,49 @@ class listener_test extends \phpbb_test_case
 	}
 
 	/**
-	* Data set for test_modify_forum_rows
+	* Data set for test_set_parent_topic_title
 	*
 	* @return array Array of test data
 	*/
-	public function modify_forum_rows_data()
+	public function set_parent_topic_title_data()
 	{
 		return array(
 			'last post of this forum is last post of parent forum' => array(
 				2,
 				array(
 					1 => array(
-						'forum_id' => 1,
-						'parent_id' => 0,
-						'forum_id_last_post' => 2,
-						'topic_title' => 'some topic title that has to be replaced',
+						'forum_id'				=> 1,
+						'parent_id'				=> 0,
+						'forum_id_last_post'	=> 2,
+						'last_post_topic_title'	=> 'some topic title that has to be replaced',
 					),
 					2 => array(
-						'forum_id' => 2,
-						'parent_id' => 1,
-						'topic_title' => 'new topic title',
+						'forum_id'				=> 2,
+						'parent_id'				=> 1,
+						'last_post_topic_title'	=> 'new topic title',
 					),
 					3 => array(
-						'forum_id' => 3,
-						'parent_id'	=> 1,
-						'topic_title' => 'topic title of some unrelated forum',
+						'forum_id'				=> 3,
+						'parent_id'				=> 1,
+						'last_post_topic_title'	=> 'topic title of some unrelated forum',
 					),
 				),
 				array(
 					1 => array(
-						'forum_id' => 1,
-						'parent_id' => 0,
-						'forum_id_last_post' => 2,
-						'topic_title' => 'new topic title',
+						'forum_id'				=> 1,
+						'parent_id'				=> 0,
+						'forum_id_last_post'	=> 2,
+						'last_post_topic_title' => 'new topic title',
 					),
 					2 => array(
-						'forum_id' => 2,
-						'parent_id' => 1,
-						'topic_title' => 'new topic title',
+						'forum_id'				=> 2,
+						'parent_id'				=> 1,
+						'last_post_topic_title'	=> 'new topic title',
 					),
 					3 => array(
-						'forum_id' => 3,
-						'parent_id'	=> 1,
-						'topic_title' => 'topic title of some unrelated forum',
+						'forum_id'				=> 3,
+						'parent_id'				=> 1,
+						'last_post_topic_title'	=> 'topic title of some unrelated forum',
 					),
 				),
 			),
@@ -349,38 +296,38 @@ class listener_test extends \phpbb_test_case
 				2,
 				array(
 					1 => array(
-						'forum_id' => 1,
-						'parent_id' => 0,
-						'forum_id_last_post' => 3,
-						'topic_title' => 'some topic title that has NOT to be replaced',
+						'forum_id'				=> 1,
+						'parent_id'				=> 0,
+						'forum_id_last_post'	=> 3,
+						'topic_title'			=> 'some topic title that has NOT to be replaced',
 					),
 					2 => array(
-						'forum_id' => 2,
-						'parent_id' => 1,
-						'topic_title' => 'topic title',
+						'forum_id'		=> 2,
+						'parent_id'		=> 1,
+						'topic_title'	=> 'topic title',
 					),
 					3 => array(
-						'forum_id' => 3,
-						'parent_id'	=> 1,
-						'topic_title' => 'topic title of some unrelated forum',
+						'forum_id'		=> 3,
+						'parent_id'		=> 1,
+						'topic_title'	=> 'topic title of some unrelated forum',
 					),
 				),
 				array(
 					1 => array(
-						'forum_id' => 1,
-						'parent_id' => 0,
-						'forum_id_last_post' => 3,
-						'topic_title' => 'some topic title that has NOT to be replaced',
+						'forum_id'				=> 1,
+						'parent_id'				=> 0,
+						'forum_id_last_post'	=> 3,
+						'topic_title'			=> 'some topic title that has NOT to be replaced',
 					),
 					2 => array(
-						'forum_id' => 2,
-						'parent_id' => 1,
-						'topic_title' => 'topic title',
+						'forum_id'		=> 2,
+						'parent_id'		=> 1,
+						'topic_title'	=> 'topic title',
 					),
 					3 => array(
-						'forum_id' => 3,
-						'parent_id'	=> 1,
-						'topic_title' => 'topic title of some unrelated forum',
+						'forum_id'		=> 3,
+						'parent_id'		=> 1,
+						'topic_title'	=> 'topic title of some unrelated forum',
 					),
 				),
 			),
@@ -390,14 +337,14 @@ class listener_test extends \phpbb_test_case
 	/**
 	* Test the core.display_forums_modify_forum_rows event
 	*
-	* @dataProvider modify_forum_rows_data
+	* @dataProvider set_parent_topic_title_data
 	*/
-	public function test_modify_forum_rows($forum_id, $forum_rows, $expected)
+	public function test_set_parent_topic_title($forum_id, $forum_rows, $expected)
 	{
 		$this->set_listener();
 
 		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-		$dispatcher->addListener('core.display_forums_modify_forum_rows', array($this->listener, 'modify_forum_rows'));
+		$dispatcher->addListener('core.display_forums_modify_forum_rows', array($this->listener, 'set_parent_topic_title'));
 
 		$row = $forum_rows[$forum_id];
 		$parent_id = $row['parent_id'];
@@ -418,59 +365,189 @@ class listener_test extends \phpbb_test_case
 	public function set_custom_last_post_data()
 	{
 		return array(
-			array(
-				'text',
-				'expected - user',
-				'expected - admin',
+			'always display last post subject' => array(
+				0,
+				array(
+					1 => array(
+						'forum_id'					=> 1,
+						'forum_last_post_subject'	=> 'post subject',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+				array(
+					1 => array(
+						'forum_id'					=> 1,
+						'forum_last_post_subject'	=> 'post subject',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+			),
+			'always display topic title' => array(
+				1,
+				array(
+					2 => array(
+						'forum_id'					=> 2,
+						'forum_last_post_subject'	=> 'post subject',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+				array(
+					2 => array(
+						'forum_id'					=> 2,
+						'forum_last_post_subject'	=> 'topic title',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+			),
+			'display topic title if last post subject is empty, last post subject is not empty' => array(
+				2,
+				array(
+					3 => array(
+						'forum_id'					=> 3,
+						'forum_last_post_subject'	=> 'post subject',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+				array(
+					3 => array(
+						'forum_id'					=> 3,
+						'forum_last_post_subject'	=> 'post subject',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+			),
+			'display topic title if last post subject is empty, last post subject is empty' => array(
+				2,
+				array(
+					4 => array(
+						'forum_id'					=> 4,
+						'forum_last_post_subject'	=> '',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
+				array(
+					4 => array(
+						'forum_id'					=> 4,
+						'forum_last_post_subject'	=> 'topic title',
+						'last_post_topic_title'		=> 'topic title',
+					),
+				),
 			),
 		);
 	}
 
 	/**
-	* Test the core.display_forums_modify_template_vars event
+	* Test the core.display_forums_before event
 	*
 	* @dataProvider set_custom_last_post_data
 	*/
-	public function test_set_custom_last_post($text, $expected_user = false, $expected_admin = false)
+	public function test_set_custom_last_post($option, $forum_rows, $expected)
 	{
 		$this->set_listener();
 
-		$this->set_auth($this->auth_acl_map_user);
+		$this->config['martin_emptypostsubjects_last_post'] = $option;
 
 		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-		$dispatcher->addListener('core.display_forums_modify_template_vars', array($this->listener, 'set_custom_last_post'));
+		$dispatcher->addListener('core.display_forums_before', array($this->listener, 'set_custom_last_post'));
 
-		$event_data = array('text');
+		$event_data = array('forum_rows');
 		$event = new \phpbb\event\data(compact($event_data));
-		$dispatcher->dispatch('core.display_forums_modify_template_vars', $event);
+		$dispatcher->dispatch('core.display_forums_before', $event);
 
 		$event_data_after = $event->get_data_filtered($event_data);
-		$this->assertArrayHasKey('text', $event_data_after);
-		$this->assertEquals(($expected_user !== false ? $expected_user : $text), $event_data_after['text']);
-
-		// TODO last post ist in unterforum, dass user nicht lesen kann
-
-		/* TODO
-		if ($expected_admin !== false) {
-			$this->create_auth();
-			$this->set_listener();
-			$this->set_auth($this->auth_acl_map_admin);
-
-			$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-			$dispatcher->addListener('core.modify_text_for_display_after', array($this->listener, 'modify_post_data'));
-
-			$event_data = array('text');
-			$event = new \phpbb\event\data(compact($event_data));
-			$dispatcher->dispatch('core.modify_text_for_display_after', $event);
-
-			$event_data_after = $event->get_data_filtered($event_data);
-			$this->assertArrayHasKey('text', $event_data_after);
-
-			$this->assertEquals($expected_admin, $event_data_after['text']);
-		}
-		*/
+		$this->assertArrayHasKey('forum_rows', $event_data_after);
+		$this->assertEquals($expected, $event_data_after['forum_rows']);
 	}
 
-	// TODO: 'core.search_modify_tpl_ary'				=> 'modify_search_results',
+	/**
+	* Data set for test_modify_search_results
+	*
+	* @return array Array of test data
+	*/
+	public function modify_search_results_data()
+	{
+		return array(
+			'do not modify search results when viewing results as topics' => array(
+				'topics',
+				0,
+				array(
+					'idem'
+				),
+				array(
+					'idem'
+				),
+			),
+			'always display post subject' => array(
+				'posts',
+				0,
+				array(
+					'POST_SUBJECT'	=> 'post subject',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+				array(
+					'POST_SUBJECT'	=> 'post subject',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+			),
+			'always display topic title' => array(
+				'posts',
+				1,
+				array(
+					'POST_SUBJECT'	=> 'post subject',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+				array(
+					'POST_SUBJECT'	=> 'topic title',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+			),
+			'display topic title if post subject is empty, post subject is not empty' => array(
+				'posts',
+				2,
+				array(
+					'POST_SUBJECT'	=> 'post subject',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+				array(
+					'POST_SUBJECT'	=> 'post subject',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+			),
+			'display topic title if post subject is empty, post subject is empty' => array(
+				'posts',
+				2,
+				array(
+					'POST_SUBJECT'	=> '',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+				array(
+					'POST_SUBJECT'	=> 'topic title',
+					'TOPIC_TITLE'	=> 'topic title',
+				),
+			),
+		);
+	}
 
+	/**
+	* Test the core.search_modify_tpl_ary event
+	*
+	* @dataProvider modify_search_results_data
+	*/
+	public function test_modify_search_results($show_results, $option, $tpl_ary, $expected)
+	{
+		$this->set_listener();
+
+		$this->config['martin_emptypostsubjects_search'] = $option;
+
+		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+		$dispatcher->addListener('core.search_modify_tpl_ary', array($this->listener, 'modify_search_results'));
+
+		$event_data = array('show_results', 'tpl_ary');
+		$event = new \phpbb\event\data(compact($event_data));
+		$dispatcher->dispatch('core.search_modify_tpl_ary', $event);
+
+		$event_data_after = $event->get_data_filtered($event_data);
+		$this->assertArrayHasKey('tpl_ary', $event_data_after);
+		$this->assertEquals($expected, $event_data_after['tpl_ary']);
+	}
 }
